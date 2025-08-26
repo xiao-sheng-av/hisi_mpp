c08057b28a74431f5ac95051d88d2a8dea815761
xiao-sheng-av  <13435249798@163.com>
2025-08-27 00:05:15 +0800

    初始化完成，但是还未能获取到数据

--- src/test.cpp
+++ src/test.cpp
@@ -1,14 +1,35 @@
 #include <iostream>
 #include "vi.hpp"
+#include "vpss.hpp"
 int main()
 {
 
     std::cout << "Hello world" << std::endl;
-    Hi_Mpp_Vi use_mpp;
-    if (use_mpp.Init())
-        return 0;
-    else return -1;
-
+    Hi_Mpp_Vi vi;
+    Hi_Mpp_Vpss vpss;
+    if (vi.Init() == false)
+    {
+        return -1;
+    }
+    if (vpss.Init() == false)
+    {
+        return -1;
+    }
+    for (int i = 0; i < 30; i++)
+    {
+        VIDEO_FRAME_INFO_S stFrameInfom;
+        HI_U32 ret = HI_MPI_VPSS_GetChnFrame(0, 0, &stFrameInfom, 30);
+        if (HI_SUCCESS != ret)
+        {
+            std::cout << "get frame false  ret = " << ret << std::endl ;
+        }
+        std::cout << "i = " << i << std::endl;
+        ret = HI_MPI_VPSS_ReleaseChnFrame(0, 0, &stFrameInfom);
+        if (HI_SUCCESS != ret)
+        {
+            std::cout << "release frame false\n";
+        }
+    }
 
 
 
