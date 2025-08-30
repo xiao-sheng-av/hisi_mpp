c791067fffe581d998b48e71d9576b62716b218b
xiao-sheng-av  <13435249798@163.com>
2025-08-30 23:00:42 +0800

    优化初始化

--- src/test.cpp
+++ src/test.cpp
@@ -1,20 +1,34 @@
 #include <iostream>
 #include "vi.hpp"
 #include "vpss.hpp"
+bool Init(Hi_Mpp_Vi & vi, Hi_Mpp_Vpss & vpss)
+{
+    HI_S32 Pipe_Id = vi.GetPipeId();
+    HI_S32 Chn_Id = vi.GetChnId();
+    if (!vi.Init())
+    {
+        std::cout << "vi Init failed\n";
+        return false;
+    }
+    if (!vpss.Init())
+    {
+        std::cout << "vpss Init failed\n";
+        return false;
+    }
+    if (!vpss.Vpss_Bind_Vi(Pipe_Id, Chn_Id))
+    {
+        std::cout << "vpss bind vi failed\n";
+        return false;
+    }
+    return true;
+}
 int main()
 {
 
     std::cout << "Hello world" << std::endl;
     Hi_Mpp_Vi vi;
     Hi_Mpp_Vpss vpss;
-    if (vi.Init() == false)
-    {
-        return -1;
-    }
-    if (vpss.Init() == false)
-    {
-        return -1;
-    }
+    Init(vi, vpss);
     for (int i = 0; i < 30; i++)
     {
         VIDEO_FRAME_INFO_S stFrameInfom;
