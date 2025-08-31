2cb24cf07acef360aa93402e1b94641ff31428c8
xiao-sheng-av <13435249798@163.com>
2025-08-31 15:42:56 +0800

    优化

--- src/test.cpp
+++ src/test.cpp
@@ -28,17 +28,19 @@ int main()
     std::cout << "Hello world" << std::endl;
     Hi_Mpp_Vi vi;
     Hi_Mpp_Vpss vpss;
+    HI_S32 Vpss_Id = vpss.Get_ChnId();
+    HI_S32 Vpss_Grp = vpss.Get_Grp();
     Init(vi, vpss);
     for (int i = 0; i < 30; i++)
     {
         VIDEO_FRAME_INFO_S stFrameInfom;
-        HI_U32 ret = HI_MPI_VPSS_GetChnFrame(0, 0, &stFrameInfom, -1);
+        HI_U32 ret = HI_MPI_VPSS_GetChnFrame(Vpss_Grp, Vpss_Id, &stFrameInfom, -1);
         if (HI_SUCCESS != ret)
         {
             std::cout << std::hex << "get frame false  ret = " << ret << std::endl ;
         }
         std::cout << "i = " << i << std::endl;
-        ret = HI_MPI_VPSS_ReleaseChnFrame(0, 0, &stFrameInfom);
+        ret = HI_MPI_VPSS_ReleaseChnFrame(Vpss_Grp, Vpss_Id, &stFrameInfom);
         if (HI_SUCCESS != ret)
         {
             std::cout << "release frame false\n";
