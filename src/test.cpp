3a54d878cfa279e36e97bd4d5bb70fca535719dc
xiao-sheng-av  <13435249798@163.com>
2025-09-05 18:58:21 +0800

    实现vi->vpss->vo

--- src/test.cpp
+++ src/test.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include "vi.hpp"
 #include "vpss.hpp"
-bool Init(Hi_Mpp_Vi & vi, Hi_Mpp_Vpss & vpss)
+#include "vo.hpp"
+bool Init(Hi_Mpp_Vi & vi, Hi_Mpp_Vpss & vpss, Hi_Mpp_Vo & vo)
 {
     HI_S32 Pipe_Id = vi.GetPipeId();
     HI_S32 Chn_Id = vi.GetChnId();
@@ -15,10 +16,19 @@ bool Init(Hi_Mpp_Vi & vi, Hi_Mpp_Vpss & vpss)
         std::cout << "vpss Init failed\n";
         return false;
     }
-    if (vpss.Vpss_Bind_Vi(Pipe_Id, Chn_Id) == false)
+    if (vpss.Bind_Vi(Pipe_Id, Chn_Id) == false)
     {
         std::cout << "vpss bind vi failed\n";
         return false;
+    }
+    if (vo.Init() != HI_SUCCESS)
+    {
+        std::cout << "vo Init failed\n";
+        return false;
+    }
+    if (vo.Bind_Vpss(vpss.Get_Grp(), vpss.Get_ChnId()) != true)
+    {
+        std::cout << "vpss bind vo false\n";
     }
     return true;
 }
@@ -28,28 +38,17 @@ int main()
     std::cout << "Hello world" << std::endl;
     Hi_Mpp_Vi vi;
     Hi_Mpp_Vpss vpss;
+    Hi_Mpp_Vo vo;
     HI_S32 Vpss_Chn = vpss.Get_ChnId();
     HI_S32 Vpss_Grp = vpss.Get_Grp();
-    if (Init(vi, vpss) == false)
+    if (Init(vi, vpss, vo) == false)
     {
         std::cout << "Init false\n";
         return -1;
     }
-    for (int i = 0; i < 30; i++)
-    {
-        VIDEO_FRAME_INFO_S stFrameInfom;
-        HI_U32 ret = HI_MPI_VPSS_GetChnFrame(Vpss_Grp, Vpss_Chn, &stFrameInfom, -1);
-        if (HI_SUCCESS != ret)
-        {
-            std::cout << std::hex << "get frame false  ret = " << ret << std::endl ;
-        }
-        std::cout << "i = " << i << std::endl;
-        vpss.Write_Frame(&stFrameInfom.stVFrame);
-        ret = HI_MPI_VPSS_ReleaseChnFrame(Vpss_Grp, Vpss_Chn, &stFrameInfom);
-        if (HI_SUCCESS != ret)
-        {
-            std::cout << "release frame false\n";
-        }
-    }
+
+    getchar();
+
+    
     return 0;
 }
